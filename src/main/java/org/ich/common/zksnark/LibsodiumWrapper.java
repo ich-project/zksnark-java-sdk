25642e097ed1116b2025ee1dc72e06db27b89751
Olabode Adebayo <webframes@gmail.com>
2021-06-03 17:10:03 +0100

    initial commit

---
+++ src/main/java/org/ich/common/zksnark/LibsodiumWrapper.java
@@ -0,0 +1,21 @@
+package org.ich.common.zksnark;
+
+import java.io.IOException;
+import org.ich.common.util.Utils;
+
+public class LibsodiumWrapper {
+  private static final Libsodium INSTANCE = new Libsodium();
+
+  static {
+    try {
+      System.load(Utils.getLibraryByName("libzksnarkjni"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static Libsodium getInstance() {
+    return INSTANCE;
+  }
+
+}
